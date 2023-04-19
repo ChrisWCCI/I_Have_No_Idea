0c488b982cc5832bfab3498ab3508c56cb5910ca
Chris Ryan <chrisryan@wecancodeit.org>
2023-04-18 22:37:03 -0400

    Added JUnit tests

--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,23 +1,29 @@
 package virtual_pet;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class VirtualPetTest {
 
+    @Test
     public void testFeed() {
         VirtualPet pet = new VirtualPet("Fido");
         int initialHungerLevel = pet.getHungerLevel();
         pet.feed();
         int newHungerLevel = pet.getHungerLevel();
-        System.out.println(newHungerLevel < initialHungerLevel);
+        assertTrue(newHungerLevel < initialHungerLevel);
     }
 
+    @Test
     public void testWater() {
         VirtualPet pet = new VirtualPet("Fido");
         int initialThirstLevel = pet.getThirstLevel();
         pet.water();
         int newThirstLevel = pet.getThirstLevel();
-        System.out.println(newThirstLevel < initialThirstLevel);
+        assertTrue(newThirstLevel < initialThirstLevel);
     }
 
+    @Test
     public void testPlay() {
         VirtualPet pet = new VirtualPet("Fido");
         int initialHungerLevel = pet.getHungerLevel();
@@ -25,10 +31,11 @@ public class VirtualPetTest {
         pet.play();
         int newHungerLevel = pet.getHungerLevel();
         int newThirstLevel = pet.getThirstLevel();
-        System.out.println(newHungerLevel <= initialHungerLevel && newHungerLevel >= initialHungerLevel - 5);
-        System.out.println(newThirstLevel <= initialThirstLevel && newThirstLevel >= initialThirstLevel - 5);
+        assertTrue(newHungerLevel <= initialHungerLevel && newHungerLevel >= initialHungerLevel - 5);
+        assertTrue(newThirstLevel <= initialThirstLevel && newThirstLevel >= initialThirstLevel - 5);
     }
 
+    @Test
     public void testTick() {
         VirtualPet pet = new VirtualPet("Fido");
         int initialHungerLevel = pet.getHungerLevel();
@@ -38,8 +45,8 @@ public class VirtualPetTest {
         int newHungerLevel = pet.getHungerLevel();
         int newThirstLevel = pet.getThirstLevel();
         int newBoredomLevel = pet.getBoredomLevel();
-        System.out.println(newHungerLevel >= initialHungerLevel && newHungerLevel <= initialHungerLevel + 5);
-        System.out.println(newThirstLevel >= initialThirstLevel && newThirstLevel <= initialThirstLevel + 5);
-        System.out.println(newBoredomLevel >= initialBoredomLevel && newBoredomLevel <= initialBoredomLevel + 5);
+        assertTrue(newHungerLevel >= initialHungerLevel && newHungerLevel <= initialHungerLevel + 5);
+        assertTrue(newThirstLevel >= initialThirstLevel && newThirstLevel <= initialThirstLevel + 5);
+        assertTrue(newBoredomLevel >= initialBoredomLevel && newBoredomLevel <= initialBoredomLevel + 5);
     }
 }
